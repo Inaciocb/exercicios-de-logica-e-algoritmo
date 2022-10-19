6dc2571397c9c14ce7b849a0c6e296dd0b28f1a9
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-10-19 13:51:33 -0300

    Create pares.c

---
+++ pares.c
@@ -0,0 +1,23 @@
+/*Crie um algoritmo que servirá para imprimir uma quantidade de números pares para serem
+impressos (a entrada mínima é 2). Exemplo de saída para a entrada 4: 2 4 6 8.*/
+
+#include <stdio.h>
+
+int main() {
+    
+    int numero, i=0;
+    
+    printf("Digite um número: ");
+    scanf("%d", &numero);
+    
+    for(;i<=(2*numero);i=i+2)
+    {
+        if(i%2==0 && i!=0)
+        {
+            printf("%d\t", i);
+        }
+    }
+    
+    
+    return 0;
+}
